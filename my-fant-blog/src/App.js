7bd7b66fbed6a4a469c9e98cf11677c131a61f05
bweatherly-bw <brandon.weatherly58@gmail.com>
2022-01-05 05:21:59 -0500

    new react blogging site/project

---
+++ my-fant-blog/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Brandons's Blog. Please login and to speak your mind!
+        </p>
+        <Button variant="dark">Get to Blogging</Button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
